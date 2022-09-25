a9c7a10cd5e70ef2f4d094afe11432b58022179f
Thunder X <72381397+Yash200717@users.noreply.github.com>
2022-09-25 10:58:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "react-native";
+import PickImage from "../screens/camera";
+
+export default class App extends React.Component{
+  render(){
+    return <PickImage />
+    }
+  }
